1c8568fd17a5a8fbc2555ab2cfebf0659918ea50
Luke Triantafyllidis <ltriant@cpan.org>
2012-11-06 14:53:53 +1100

    crypto/ dir and simple rc4 implementation

---
+++ crypto/rc4.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/env perl
+
+# rc4.pl
+#
+# usage:
+#   echo foobar | rc4.pl <key>
+
+use warnings;
+use strict;
+
+my @key = map ord, split //, shift;
+my @k = (@key) x (int(256 / @key) + 1);
+@k = @k[0..255];
+
+my @s = (0 .. 255);
+my $j = 0;
+for my $i (0 .. 255) {
+	$j = ($j + $s[$i] + $k[$i]) % 256;
+	@s[$i,$j] = @s[$j,$i];
+}
+
+$j = 0;
+my $i = 0;
+while (sysread STDIN, my $bytes, 1024) {
+	my $buffer = '';
+	for my $byte (split //, $bytes) {
+		$i = ($i + 1) % 256;
+		$j = ($j + $s[$i]) % 256;
+		@s[$i,$j] = @s[$j,$i];
+		my $t = ($s[$i] + $s[$j]) % 256;
+		my $k = $s[$t];
+		$buffer .= $byte ^ chr($k);
+	}
+	syswrite STDOUT, $buffer;
+}
+
