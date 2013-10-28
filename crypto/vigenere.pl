9de243de3ff105ff75aad7e17227c92881a4fe1a
Luke Triantafyllidis <ltriant@cpan.org>
2013-10-28 21:23:36 +1100

    implement Autokey cipher aswell

--- crypto/vigenere.pl
+++ crypto/vigenere.pl
@@ -1,10 +1,14 @@
 #!/usr/bin/env perl
-# vigenere.pl
+# vigenere.pl -- Vigenere and Autokey ciphers
 # usage:
-#   $ perl vigenere.pl foo 'foo bar baz quux'
-#   KCC GOF GON VIIC
-#   $ perl vigenere.pl -d foo 'KCC GOF GON VIIC'
-#   FOO BAR BAZ QUUX
+#   $ perl vigenere.pl --key foo 'bar baz quux'
+#   GOF GON VIIC
+#   $ perl vigenere.pl -d --key foo 'GOF GON VIIC'
+#   BAR BAZ QUUX
+#   $ perl vigenere.pl --autokey foo 'bar baz quux'
+#   GOF CAQ RUTN
+#   $ perl vigenere.pl -d --autokey foo 'GOF CAQ RUTN'
+#   BAR BAZ QUUX
 
 use warnings;
 use strict;
@@ -17,12 +21,11 @@ use constant {
 	RIGHT => 1,
 };
 
-my $encrypt = 0;
-my $decrypt = 0;
-
 GetOptions(
-	'e|encrypt!' => \$encrypt,
-	'd|decrypt!' => \$decrypt,
+	'e|encrypt!'  => \my $encrypt,
+	'd|decrypt!'  => \my $decrypt,
+	'k|key=s'     => \my $key,
+	'a|autokey=s' => \my $autokey,
 );
 
 # default to encryption
@@ -37,7 +40,7 @@ sub rotate {
 sub rol { rotate(LEFT, @_) }
 sub ror { rotate(RIGHT, @_) }
 
-my $lookup = [
+my $tab_recta = [
 	map {
 		[ rol(ord($_) - 65, 'A' .. 'Z') ]
 	} 'A' .. 'Z'
@@ -45,18 +48,29 @@ my $lookup = [
 
 sub encrypt {
 	my ($k, $d) = @_;
-	$lookup->[ord($k) - 65]->[ord($d) - 65];
+	$tab_recta->[ord($k) - 65]->[ord($d) - 65];
 }
 
 sub decrypt {
 	my ($k, $d) = @_;
-	$lookup->[(26 - (ord($k) - 65)) % 26]->[ord($d) - 65]
+	$tab_recta->[(26 - (ord($k) - 65)) % 26]->[ord($d) - 65]
 }
 
-my $key = shift || die "no key";
+die "no key" unless $key or $autokey;
+
 my $data = @ARGV ? shift : slurp(\*STDIN);
 
-$key =~ s/[^a-zA-Z]//g;
+if ($autokey) {
+	if ($encrypt) {
+		$key = $autokey . $data;
+	}
+	elsif ($decrypt) {
+		$key = $autokey;
+	}
+}
+
+$key = uc $key;
+$key =~ s/[^A-Z]//g;
 
 my $ki = 0;
 for my $x (0 .. length($data) - 1) {
@@ -67,13 +81,18 @@ for my $x (0 .. length($data) - 1) {
 		next;
 	}
 
-	my $k = uc substr $key, $ki++ % length($key), 1;
+	my $k = substr $key, $ki++ % length($key), 1;
 
 	if ($encrypt) {
 		print encrypt($k,$d);
 	}
 	elsif ($decrypt) {
-		print decrypt($k,$d);
+		# feed decrypted text back into the key if we're doing Autokey cipher
+		# stuff.
+		my $c = decrypt($k,$d);
+		$key .= $c if $autokey;
+
+		print $c;
 	}
 }
 print "\n";
