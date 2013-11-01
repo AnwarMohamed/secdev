df7594c83b617fdbd77f19e471c918401e68a312
Luke Triantafyllidis <ltriant@cpan.org>
2013-11-01 22:05:54 +1100

    pack/unpack instead of string ops

--- crypto/rc4.pl
+++ crypto/rc4.pl
@@ -3,34 +3,32 @@
 # rc4.pl
 #
 # usage:
-#   echo foobar | rc4.pl <key>
+#   echo foobar | rc4.pl <key as hex string>
 
 use warnings;
 use strict;
 
-my @key = map ord, split //, (shift or die "no key");
-my @k = (@key) x (int(256 / @key) + 1);
-@k = @k[0..255];
+my @k = unpack "C*", pack "H*", (shift or die "no key");
 
 my @s = (0 .. 255);
 my $j = 0;
 for my $i (0 .. 255) {
-	$j = ($j + $s[$i] + $k[$i]) & 0xff;
+	$j = ($j + $s[$i] + $k[$i%@k]) & 0xff;
 	@s[$i,$j] = @s[$j,$i];
 }
 
 $j = 0;
 my $i = 0;
-while (sysread STDIN, my $bytes, 1024) {
-	my $buffer = '';
-	for my $byte (split //, $bytes) {
+while (sysread STDIN, my $bytes, 4096) {
+	my @buf;
+	for my $b (unpack "C*", $bytes) {
 		$i = ($i + 1) & 0xff;
 		$j = ($j + $s[$i]) & 0xff;
 		@s[$i,$j] = @s[$j,$i];
 		my $t = ($s[$i] + $s[$j]) & 0xff;
 		my $k = $s[$t];
-		$buffer .= $byte ^ chr($k);
+		push @buf => $b ^ $k;
 	}
-	syswrite STDOUT, $buffer;
+	syswrite STDOUT, pack("C*", @buf);
 }
 
